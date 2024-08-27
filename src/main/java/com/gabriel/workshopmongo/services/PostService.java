00c0246363a56a915f612cdd34e100083ae72289
Alexandroni07 <gaalexandroni@gmail.com>
2024-08-26 22:34:57 -0300

    return post with ID

---
+++ src/main/java/com/gabriel/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.gabriel.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabriel.workshopmongo.domain.Post;
+import com.gabriel.workshopmongo.repository.PostRepository;
+import com.gabriel.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
