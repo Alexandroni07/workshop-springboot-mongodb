d7242e04e3486a9be92da1e816b2f8381a60197b
Alexandroni07 <gaalexandroni@gmail.com>
2024-08-26 21:38:26 -0300

    Update PUT

--- src/main/java/com/gabriel/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gabriel/workshopmongo/resources/UserResource.java
@@ -46,4 +46,12 @@ public class UserResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@PutMapping("/{id}")
+ 	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
 }
