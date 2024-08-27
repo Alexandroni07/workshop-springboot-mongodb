0635d1c61f4cfca81023cee0e789a3c0732ccc52
Alexandroni07 <gaalexandroni@gmail.com>
2024-08-26 22:44:17 -0300

    post comments

--- src/main/java/com/gabriel/workshopmongo/domain/Post.java
+++ src/main/java/com/gabriel/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.gabriel.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.gabriel.workshopmongo.dto.AuthorDTO;
+import com.gabriel.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -18,6 +21,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public Post() {
 	}
@@ -70,6 +75,15 @@ public class Post implements Serializable {
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
 	
 	@Override
 	public int hashCode() {
