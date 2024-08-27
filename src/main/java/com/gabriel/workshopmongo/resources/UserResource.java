30579be3c33dcabd917b2c198b739d9dac8c5477
Alexandroni07 <gaalexandroni@gmail.com>
2024-08-26 21:25:45 -0300

    User delete

--- src/main/java/com/gabriel/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gabriel/workshopmongo/resources/UserResource.java
@@ -41,4 +41,9 @@ public class UserResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
+	@DeleteMapping("/{id}")
+ 	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
